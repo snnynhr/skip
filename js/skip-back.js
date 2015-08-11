66a33346fce5433369b908326b56680d693b2ab7
Sunny Nahar <sunnyn@cmu.edu>
2015-08-10 22:41:51 -0700

    support for null prevs

---
+++ js/skip-back.js
@@ -0,0 +1,7 @@
+console.log('hi');
+var inp = document.getElementsByClassName('ytp-prev-button ytp-button');
+if (inp.length > 0 && inp[0].getAttribute('aria-disabled') == 'false') {
+    inp[0].click();
+} else {
+    history.back();
+}
