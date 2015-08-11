25c8d6060693304a37fc5379cfd9c19fb68857b6
Sunny Nahar <sunnyn@cmu.edu>
2015-08-10 22:40:33 -0700

    updated build

--- build.bat
+++ build.bat
@@ -2,10 +2,13 @@ rmdir build /S /Q
 mkdir build
 CD build/
 mkdir js
-mkdir util
+mkdir icons
 CD ..
-XCOPY util\jquery.min.js build\util
 XCOPY manifest.json build
+XCOPY background.html build
+XCOPY icons\* build\icons
 call compile\minifyjs.bat
 call compile\update_crx.bat
 call compile\zip.bat
+move skip.zip "build/skip.zip"
+move skip.crx "build/skip.crx"
